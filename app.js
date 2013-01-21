07681e81fd62b64ea20e2c0975089f1f110ad6a1
moonrise <bjspicy@yahoo.com>
2013-01-20 16:14:23 -0800

    sencha touch test

---
+++ app.js
@@ -0,0 +1,192 @@
+//<debug>
+Ext.Loader.setPath({
+    'Ext': 'touch/src',
+    'touch': 'app'
+});
+//</debug>
+
+Ext.application({
+    name: 'touch',
+
+    requires: [
+        'Ext.MessageBox'
+    ],
+
+    views: ['Main'],
+
+    icon: {
+        '57': 'resources/icons/Icon.png',
+        '72': 'resources/icons/Icon~ipad.png',
+        '114': 'resources/icons/Icon@2x.png',
+        '144': 'resources/icons/Icon~ipad@2x.png'
+    },
+
+    isIconPrecomposed: true,
+
+    startupImage: {
+        '320x460': 'resources/startup/320x460.jpg',
+        '640x920': 'resources/startup/640x920.png',
+        '768x1004': 'resources/startup/768x1004.png',
+        '748x1024': 'resources/startup/748x1024.png',
+        '1536x2008': 'resources/startup/1536x2008.png',
+        '1496x2048': 'resources/startup/1496x2048.png'
+    },
+
+    launch1: function() {
+        Ext.create("Ext.tab.Panel", {
+            fullscreen: true,
+            tabBarPosition: 'bottom',
+            items: [
+                {
+                    title: 'Home',
+                    iconsCls: 'home',
+                    cls: 'home',
+                    html: [
+                        '<img src="http://staging.sencha.com/img/sencha.png" />',
+                        '<h1>Welcome to Sencha Touch</h1>',
+                        "<p>You're creating the Getting Started app. This demonstrates how ",
+                        "to use tabs, lists and forms to create a simple app</p>",
+                        '<h2>Sencha Touch (2.1.0)</h2>'
+                    ].join("")
+                }
+            ]
+        });
+    },
+
+    launch: function() {
+        Ext.Viewport.add({
+            xtype: 'tabpanel',
+            fullscreen: true,
+            tabBarPosition: 'bottom',
+
+            items: [
+                // This is the home page, just some simple html
+                {
+                    title: 'Home',
+                    iconCls: 'home',
+                    cls: 'home',
+                    scrollable: true,
+                    html: [
+                        '<img height=260 src="http://staging.sencha.com/img/sencha.png" />',
+                        '<h1>Welcome to Sencha Touch</h1>',
+                        "<p>Building the Getting Started app</p>",
+                        '<h2>Sencha Touch (2.1)</h2>'
+                    ].join("")
+                },
+
+                // blog page
+                {
+                    xtype: 'nestedlist',
+                    title: 'Blog',
+                    iconCls: 'star',
+                    displayField: 'title',
+
+                    detailCard: {
+                        xtype: 'panel',
+                        scrollable: true,
+                        styleHtmlContent: true
+                    },
+
+                    listeners: {
+                        itemtap: function(nestedList, list, index, element, post) {
+                            this.getDetailCard().setHtml(post.get('content'));
+                        }
+                    },
+
+                    store: {
+                        type: 'tree',
+
+                        fields: [
+                            'title', 'link', 'author', 'contentSnippet', 'content',
+                            {name: 'leaf', defaultValue: true}
+                        ],
+
+                        root: {
+                            leaf: false
+                        },
+
+                        proxy: {
+                            type: 'jsonp',
+                            url: 'https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://feeds.feedburner.com/SenchaBlog',
+                            reader: {
+                                type: 'json',
+                                rootProperty: 'responseData.feed.entries'
+                            }
+                        }
+                    }
+                },
+
+                // This is the contact page, which features a form and a button. The button submits the form
+                {
+                    xtype: 'formpanel',
+                    title: 'Contact Us',
+                    iconCls: 'user',
+                    url: 'contact.php',
+                    layout: 'vbox',
+
+                    items: [
+                        {
+                            xtype: 'fieldset',
+                            title: 'Contact Us',
+                            instructions: 'Email address is optional',
+
+                            items: [
+                                {
+                                    xtype: 'textfield',
+                                    label: 'Name',
+                                    name: 'name'
+                                },
+                                {
+                                    xtype: 'emailfield',
+                                    label: 'Email',
+                                    name: 'email'
+                                },
+                                {
+                                    xtype: 'textareafield',
+                                    label: 'Message',
+                                    name: 'message',
+                                    height: 90
+                                }
+                            ]
+                        },
+                        {
+                            xtype: 'button',
+                            text: 'Send',
+                            ui: 'confirm',
+
+                            // The handler is called when the button is tapped
+                            handler: function() {
+
+                                // This looks up the items stack above, getting a reference to the first form it see
+                                var form = this.up('formpanel');
+
+                                // Sends an AJAX request with the form data to the url specified above (contact.php).
+                                // The success callback is called if we get a non-error response from the server
+                                form.submit({
+                                    success: function() {
+                                        // The callback function is run when the user taps the 'ok' button
+                                        Ext.Msg.alert('Thank You', 'Your message has been received', function() {
+                                            form.reset();
+                                        });
+                                    }
+                                });
+                            }
+                        }
+                    ]
+                }
+            ]
+        });
+    },
+
+    onUpdated: function() {
+        Ext.Msg.confirm(
+            "Application Update",
+            "This application has just successfully been updated to the latest version. Reload now?",
+            function(buttonId) {
+                if (buttonId === 'yes') {
+                    window.location.reload();
+                }
+            }
+        );
+    }
+});
